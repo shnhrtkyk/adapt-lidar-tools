53202d1d7bbb637dfd540ee90ab5c68ad2a7547c
ravi <ravishankar@u.boisestate.edu>
2018-12-05 15:37:19 -0700

    Install script now has options to install in a different directory

--- install.sh
+++ install.sh
@@ -1,87 +1,130 @@
 #!/bin/sh
 # Author: Ravi Shankar (rvishnkr)
 # Installer script to setup adapt-lidar-tools and build the executables
+set -e
+set -u
 
-#### Display usage
-display_usage() { 
-  printf "\nTo install adapt-lidar-tools, run the install script from the root of the cloned repository folder\n"
-	printf "Usage:\t`basename $0` [-h]\n" 
+### Prints help message
+help_message(){
+  printf "\nUsage: \n\t$(basename $0) <[-d] [-c dir] [-h>]\n"
+  printf "where:\n"
+  printf "\t-d\tInstalls executable in the default bin/ directory\n"
+  printf "\t-c dir\tInstall executable in user specified directory dir (full path required)\n"
+  printf "\t-h\tPrints usage\n\n"
+  exit 0;
 }
 
-### Exit immediately if a command exits with a non-zero status.
-set -e
+### Initializes and updates submodules
+init_update_modules(){
+  ## Initialize and update submodules
+  printf "Initializing submodules\n"
+  git submodule init
+  printf "####                      (25%%)\n"
+  sleep 2
+
+  echo "Updating submodules"
+  git submodule update
+  printf "###########               (50%%)\n"
+  sleep 2
+
+  ## Change 'atoill' to 'atoll'
+  echo "Updating PulseWaves"
+  cd deps/PulseWaves/src
+  sed -i 's/atoill/atoll/g' pulsefilter.cpp
+  sed -i 's/atoill/atoll/g' pulsetransform.cpp
+  make libpulsewaves.a
+  printf "##############            (75%%)\n"
+  sleep 2
+}
+
+### Builds all executables
+build_execs(){
+  ## Build the pls-to-geotiff driver
+  echo "Building geotiff-driver in bin/"
+  cd ../../../
+  make geotiff-driver
+  printf "################          (90%%)\n"
+  sleep 2
+
+  ## Build the pls-info tool
+  echo "Building pls-info tool in bin/"
+  make pls-info
+  printf "###################       (100%%)\n\n"
+  sleep 2
+}
+
+# Copy executables to user specified directory
+copy_exec_to_dir(){
+  if [[ ! -d "$dir" ]]
+  then
+    echo "Directory doesn't exist. Creating now"
+    mkdir -p -- "$dir"
+    echo "Directory created"
+  fi 
+  cp bin/* "$dir" 
+ 
+}
 
 ### 'trap' defines and activates handlers to be run when the shell receives 
-###	signals or other special conditions
-trap 'previous_command=$this_command; this_command=$BASH_COMMAND' DEBUG
+### signals or other special conditions
+trap 'this_command=$BASH_COMMAND; previous_command=$this_command' DEBUG
 
 ### Abort on a failed commands
 trap 'printf "\nexit $? due to:\n\t$previous_command\n"' EXIT
 
-### Check arguments
-if [ "$1" == "-h" ] || [ "$1" == "help" ]; then
-	display_usage
-  exit 0
-elif [ -z "$1" ] ### If no arguments, continue runnng the script
+# Exit if no options
+if [ $# -eq 0 ];
 then
-  echo "----------------------------"
-  echo "Setting up adapt-lidar-tools"
-  echo "----------------------------"
-else
-  printf "Invalid arguments. Exiting\n"
-  exit 1
+  help_message
+  exit 0
 fi
 
-### Initialize and update submodules
-printf "Initializing submodules\n"
-git submodule init
-printf "####                      (25%%)\n"
-sleep 2
+# Parse options
+while getopts 'dc:h' OPTION; do
+  case "$OPTION" in
+    d)
+      echo "Using default options."
+      init_update_modules
+      build_execs
+      printf "Success!\n\n"
+      printf "The executables can be found in $(pwd)/bin\n"
+      printf "To add the bin/ directory to your environment path add the following line to your bash profile: \n"
+      printf "\texport PATH=\$PATH:$(pwd)/bin\n\n"
+      printf "Installation complete!\n\n"
+      exit 0
+      ;;
 
-echo "Updating submodules"
-git submodule update
-printf "###########               (50%%)\n"
-sleep 2
+    h)
+      help_message
+      exit 0;
+      ;;
 
-### Change 'atoill' to 'atoll'
-echo "Updating PulseWaves"
-cd deps/PulseWaves/src
-sed -i 's/atoill/atoll/g' pulsefilter.cpp
-sed -i 's/atoill/atoll/g' pulsetransform.cpp
-make libpulsewaves.a
-printf "##############            (75%%)\n"
-sleep 2
-
-### Build the pls-to-geotiff driver
-echo "Building geotiff-driver in bin/"
-cd ../../../
-make geotiff-driver
-printf "################          (90%%)\n"
-sleep 2
-
-### Build the pls-info tool
-echo "Building pls-info tool in bin/"
-make pls-info
-printf "###################       (100%%)\ni\n"
-sleep 2
-
-printf "\nRun the executables from the $(pwd)/bin directory\n"
-printf "Alternately, you may add the directory to your Environment Path\n"
-
-echo "Do you wish to add $(pwd)/bin to your environment path?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes) 
-		echo "yes!"; 
-		echo "export PATH=\$PATH:$(pwd)/bin" >> ~/.bash_profile
-		source ~/.bash_profile
-		printf "\nInstallation complete.\n" 
-		printf "Please run \"source ~/.bash_profile\" or restart your session!\n"
-		break;;
-        No) 
-		echo "no"; break;;
-    esac
+    c)
+      dir="$OPTARG"
+      echo "The directory provided is $OPTARG"
+      init_update_modules
+      build_execs
+      copy_exec_to_dir
+      printf "Success!\n\n"
+      printf "Installation complete!\n\n"
+      exit 0
+      ;;
+    ?)
+	help_message
+	exit 1
+      ;;
+  esac
 done
 
-printf "\nInstallation complete. Have a nice day!\n"
-exit 0
+# Check for invalid options
+if [ $OPTIND -eq 1 ]; 
+then 
+  echo "Invalid option/ No option specified"; 
+  help_message;
+fi
+
+# Removes all the options that have been parsed by getopts from the 
+# parameters list, and so after that point, $1 will refer to the first 
+# non-option argument passed to the script.
+shift "$(($OPTIND -1))"
+exit 1
